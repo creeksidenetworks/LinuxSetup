c02dca76e815934df56ec9af4c0496a9528e7832
Jackson Tong <creeksidenetworks@gmail.com>
2024-12-15 06:37:29 +0800

    initial project

---
+++ linuxsetup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Azure AD user/group FreeIPA sync utility
+# Copyright (c) 2024 Jackson Tong, Creekside Networks LLC.
+
+# main function
+title = """
+***************************************************************************
+*                      Linux Setup Utility v1.0                           *
+*          (c) 2024-2024 Jackson Tong, Creekside Networks LLC             *
+***************************************************************************
+
+"""
+def main():
+
+    # main scripts started here
+    print(title)
+
+if __name__ == "__main__":
+    main()
